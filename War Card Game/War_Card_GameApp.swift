98f6d38c40ce1b04a4f03459321098f8693e807a
Nykuluz <carlmillanes3@gmail.com>
2023-07-03 11:03:07 +0800

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Carl Irven Nykuluz F. Millanes on 7/3/23.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
